9f5c1d3f54a8915d5a738da4b69373ccf9c6b188
louistrankhanhhung-arch <louis.trankhanhhung@gmail.com>
2025-08-07 17:44:07 +0700

    Update block_runner.py

--- block_runner.py
+++ block_runner.py
@@ -6,7 +6,7 @@ blocks = ["block1", "block2", "block3"]
 for block in blocks:
     print(f"\n🚀 Running {block}...")
     try:
-        subprocess.run(["python", "main.py", block], check=True)
+        subprocess.run(["python", "main_testable_final.py", block], check=True)
     except subprocess.CalledProcessError as e:
         print(f"❌ Lỗi khi chạy {block}: {e}")
     time.sleep(60)  # nghỉ 5 phút giữa các block
